66fd1469d55fe1e1687e8f5259240ab1a54d597e
st <st@MacBook-Pro-st.local>
2014-06-04 18:50:55 +0400

    fix

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'skyline'
+
+set :stages, ['production']
+
+set :repo_url, 'git@bitbucket.org:cwkcwk/horizont.git'
+
+set :branch, 'master'
+
+set :deploy_via, :remote_cache
+
+set :deploy_to, '/var/www/skyline'
+
+set :keep_releases, 5
+
+set :pty, true
+
+set :stage, :production
+
+set :rails_env, 'production'
+
+set :unicorn_workers, 1
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+       execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
