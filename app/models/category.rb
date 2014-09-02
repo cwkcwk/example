51301e95624723c2821abfb94f2eb1ed4307faf9
st <st@MacBook-Pro-st.local>
2014-09-02 16:38:37 +0400

    fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,7 +12,7 @@ class Category < ActiveRecord::Base
   has_attached_file :image,
                     styles: {
                         thumb: '100x100',
-                        large: '300x300#',
+                        large: '300x300',
                     }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
