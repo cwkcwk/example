c8c724c1ea72c4b657fa92602bcde20b5c03f793
st <st@MacBook-Pro-st.local>
2014-09-17 12:40:37 +0400

    fix gallery

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,20 @@
-$(function () {
-    $('.links').click(function () {
-        blueimp.Gallery($(this).find('.gallery-item'), {event: event || window.event, index: event.target.src});
-    })
+$(function(){
+    initEachSlider();
 });
+
+function initEachSlider() {
+    $('.links').each(function(){
+        initSlider(this);
+    })
+}
+
+function initSlider(collection){
+    collection.onclick = function (event) {
+        event = event || window.event;
+        var target = event.target || event.srcElement,
+            link = target.src ? target.parentNode : target,
+            options = {index: link, event: event},
+            links = this.getElementsByTagName('a');
+        blueimp.Gallery(links, options);
+    };
+}
